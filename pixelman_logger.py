23a046673b318b496ab635004459b13a580d0530
joelvogt <bullfishfighter@gmail.com>
2015-01-29 11:20:14 +0100

    Preparing the code to add async et al. features

--- pixelman_logger.py
+++ pixelman_logger.py
@@ -14,7 +14,11 @@ def write(event):
     global logfile
     logfile.write(event)
     logfile.flush()
-    # logfile.close()
+    logfile.close()
+    fd = open(logfile)
+    h2 = hash(''.join(fd.readlines()))
+    fd.close()
+    return h2
 
 
 @networked_function(buffered=False)
