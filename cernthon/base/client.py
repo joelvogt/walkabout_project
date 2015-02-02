006b7b8ffd58c0674fee58fa871adbd8ab07f047
joelvogt <bullfishfighter@gmail.com>
2015-02-02 14:47:44 +0100

    decoupling connection, serialization, ...

--- cernthon/base/client.py
+++ cernthon/base/client.py
@@ -5,14 +5,28 @@ import imp
 
 from cernthon.connection.tcpsock.client import Client
 import os
+from collections import namedtuple
 
 
 config_file = os.path.join(os.curdir, 'config.py')
-cernthon_config = None
+
 if os.path.exists(config_file):
     cernthon_config = imp.load_source('config', config_file)
 else:
-    raise AttributeError("config.py not found, the system can't be configured")
+    CernthonConfig = namedtuple('CernthonConfig', ['client_id', 'modules'])
+    client_id = 'osx_client'
+    modules = dict(
+        pixelman_logger = dict(
+            buffer_size = 4096,
+            connection = 'tcpsock',
+            serialization = dict(
+                data = 'python_pickling',
+                results = 'python_pickling'
+            )
+        )
+    )
+    cernthon_config = CernthonConfig(client_id, modules)
+    # raise AttributeError("config.py not found, the system can't be configured")
 
 
 def import_module(module_name, directory_service_hostname='127.0.0.1', port=9000):
