358a38d67e3993994db2597a950f14cd2a3979a6
joelvogt <bullfishfighter@gmail.com>
2015-02-09 11:45:23 +0100

    Testing non-blocking sockets for remote functions without return values. The return channel is not omitted, because exceptions pass through it

--- walkabout/connection/tcpsock/server.py
+++ walkabout/connection/tcpsock/server.py
@@ -127,6 +127,7 @@ class Server(object):
     def __call__(self, buffered_func, buffered=False):
         def buffered_function(func):
             def on_call(params):
+                print('debug on call')
                 return [func(*args, **kwargs) for args, kwargs in params]
 
             return on_call
