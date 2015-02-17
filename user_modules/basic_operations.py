9762d92c5bc0e0e29936e7405f5a946cc47acb83
joelvogt <bullfishfighter@gmail.com>
2015-02-17 17:08:55 +0100

    Buffering but without return values

--- user_modules/basic_operations.py
+++ user_modules/basic_operations.py
@@ -4,5 +4,10 @@ from walkabout.helpers.moduleslib import networked_function
 
 
 @networked_function(buffered=True)
+def b_doSum(arr):
+    return np.sum(arr)
+
+
+@networked_function(buffered=False)
 def doSum(arr):
     return np.sum(arr)
