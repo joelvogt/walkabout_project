deb91619e598f48c1ec6b9ae1f5fe7c07f13d605
joelvogt <bullfishfighter@gmail.com>
2015-02-18 14:44:56 +0100

    Buffering but without return values

--- user_modules/basic_operations.py
+++ user_modules/basic_operations.py
@@ -3,13 +3,12 @@ import numpy as np
 from walkabout.helpers.moduleslib import networked_function
 from walkabout.adapters.numpy_adapters import numpy_to_jython
 
+
 @networked_function(buffered=True)
-@numpy_to_jython
 def b_doSum(arr):
-    return np.sum(arr)
+    return numpy_to_jython(np.sum(arr))
 
 
 @networked_function(buffered=False)
-@numpy_to_jython
 def doSum(arr):
-    return np.sum(arr)
+    return numpy_to_jython(np.sum(arr))
--- walkabout/adapters/numpy_adapters.py
+++ walkabout/adapters/numpy_adapters.py
@@ -4,18 +4,15 @@ __author__ = u'Joël Vogt'
 import numpy
 
 
-def map_value(_value):
-    if type(_value) == numpy.ndarray:
-        return _value.tolist()
-    else:
-        return _value
+def numpy_to_jython(value):
+    def map_value(_value):
+        if type(_value) == numpy.ndarray:
+            return _value.tolist()
+        else:
+            return _value
 
-
-def numpy_to_jython(func):
-    def on_call(*args, **kwargs):
-        result = func(*args, **kwargs)
-        if type(result) in [list, tuple]:
-            return map(map_value, result)
-        return result
-
-    return on_call
+    if type(value) in [list, tuple]:
+        return map(map_value, value)
+    if numpy.issubdtype(value, numpy.dtype):
+        return value.tolist()
+    return value
