31a218b111cdfa4eb22b86fa086f2828dcff5648
joelvogt <bullfishfighter@gmail.com>
2015-02-23 15:17:25 +0100

    Changed client id from time stamp to sys.hexversion, so that the same client gets to reuse its module

--- user_modules/basic_operations.py
+++ user_modules/basic_operations.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+from walkabout.adapters.numpy_adapters import numpy_to_jython
 from walkabout.helpers.moduleslib import networked_function
 
 
 @networked_function(buffered=True)
 def b_doSum(arr):
-    return np.sum(arr)
+    return numpy_to_jython(np.sum(arr))
 
 
 @networked_function(buffered=False)
 def doSum(arr):
-    return np.sum(arr)
+    return numpy_to_jython(np.sum(arr))
