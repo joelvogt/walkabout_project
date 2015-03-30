94a09efb692e8a5cefb3e3d46f203feaf416ac77
joelvogt <bullfishfighter@gmail.com>
2015-03-30 15:49:22 +0200

    Added Walkabout-MQTT client to send pixelman frames

--- tests/mqtt_subscribe.py
+++ tests/mqtt_subscribe.py
@@ -1,18 +1,17 @@
-import paho.mqtt.client as mqtt
+from clients.walkabout_experiment import ExperimentConsumer, FrameAction
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe('experiment/testexp')
 
-def on_message(client, userdata, msg):
-    print(msg.payload)
-    # reply = raw_input('> {0} '.format(msg.topic))
-    # client.publish(msg.topic, reply)
+class DoFrame(FrameAction):
+    def __init__(self):
+        print('init')
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect('test.mosquitto.org')
+    def __call__(self, frame):
+        print(frame)
 
-# client.publish('experiment', "I'm here!")
-client.loop_forever()
+    def close(self):
+        print('bye bye')
+
+
+f = ExperimentConsumer('pixelman_data', DoFrame())
+
+f.listen()
